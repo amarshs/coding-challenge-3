d8e5b7cb48f38fde4e866384cc498292cad2264a
amarshs <152249539+amarshs@users.noreply.github.com>
2023-12-01 11:35:05 +0000

    filter option to data table

--- coding_challenge_3.py
+++ coding_challenge_3.py
@@ -71,10 +71,21 @@ with col5:
     top_rated_books = df[df['RATING'] == 5].sort_values(by='PRICE').head(5)
     st.table(top_rated_books[['TITLE', 'RATING', 'PRICE', 'AVAILABILITY']])
 
-# Display the "Book Data" table with reduced height
+# Display the "Book Data" table with reduced height and filter option
 with col6:
     st.subheader("Book Data:")
-    st.dataframe(df, height=205, width= 1000)
+    
+    # Sidebar for filtering options
+    with st.sidebar:
+        st.sidebar.subheader("Filter Options:")
+        min_price = st.sidebar.slider("Minimum Price", min_value=df['PRICE'].min(), max_value=df['PRICE'].max(), value=df['PRICE'].min())
+        max_price = st.sidebar.slider("Maximum Price", min_value=df['PRICE'].min(), max_value=df['PRICE'].max(), value=df['PRICE'].max())
+    
+    # Filter the dataframe based on user's selection
+    filtered_df = df[(df['PRICE'] >= min_price) & (df['PRICE'] <= max_price)]
+    
+    # Display the filtered dataframe
+    st.dataframe(filtered_df, height=205, width=1000)
 
 # Create a layout with 3 columns for histogram, pie chart, and line plot
 col7, col8, col9 = st.columns([1, 1, 1])
